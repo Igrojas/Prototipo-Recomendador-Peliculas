191e3d39bd356887ef59e4e9cb53e3cf876a8177
Ignacio Rojas <igrojasro@hotmail.com>
2024-11-02 15:38:38 -0300

    Update app.py

--- app.py
+++ app.py
@@ -25,34 +25,32 @@ if st.button("enviar"):
 
     credentials_dict = st.secrets["gcp_service_account"]
 
-    # Usar las credenciales
-    print(credentials_dict["project_id"])
 
-    # print(f"Agregando recomendaciones a excel")
+    print(f"Agregando recomendaciones a excel")
 
-    # # Configuración para autenticación y acceso a Google Sheets
-    # scope = ["https://spreadsheets.google.com/feeds", 
-    #         "https://www.googleapis.com/auth/spreadsheets", 
-    #         "https://www.googleapis.com/auth/drive.file", 
-    #         "https://www.googleapis.com/auth/drive"]
+    # Configuración para autenticación y acceso a Google Sheets
+    scope = ["https://spreadsheets.google.com/feeds", 
+            "https://www.googleapis.com/auth/spreadsheets", 
+            "https://www.googleapis.com/auth/drive.file", 
+            "https://www.googleapis.com/auth/drive"]
 
     # credentials = ServiceAccountCredentials.from_json_keyfile_dict(json.loads(st.secrets["GOOGLE_APPLICATION_CREDENTIALS"]))
 
-    # client = gspread.authorize(credentials)
+    client = gspread.authorize(credentials_dict)
 
-    # # Abrir la hoja de cálculo usando su URL
-    # spreadsheet_url = 'https://docs.google.com/spreadsheets/d/1T2H33PS-I0g0PhQyYblMrYcc8K3FSLbEkonV0VDYfwI/edit?usp=sharing'
+    # Abrir la hoja de cálculo usando su URL
+    spreadsheet_url = 'https://docs.google.com/spreadsheets/d/1T2H33PS-I0g0PhQyYblMrYcc8K3FSLbEkonV0VDYfwI/edit?usp=sharing'
 
-    # spreadsheet = client.open_by_url(spreadsheet_url)
+    spreadsheet = client.open_by_url(spreadsheet_url)
 
-    # # --------------------------------------------------------- #
-    # worksheet_entrada = spreadsheet.worksheet("Nuevo Ingreso")
+    # --------------------------------------------------------- #
+    worksheet_entrada = spreadsheet.worksheet("Nuevo Ingreso")
 
-    # pelicula_1 = pelicula1
-    # pelicula_2 = pelicula2
-    # pelicula_3 = pelicula3
+    pelicula_1 = pelicula1
+    pelicula_2 = pelicula2
+    pelicula_3 = pelicula3
 
 
-    # # Insertar datos en una nueva fila
-    # new_row = [mail, pelicula_1, pelicula_2, pelicula_3]
-    # worksheet_entrada.append_row(new_row)
+    # Insertar datos en una nueva fila
+    new_row = [mail, pelicula_1, pelicula_2, pelicula_3]
+    worksheet_entrada.append_row(new_row)
