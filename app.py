9370db7eccf5f7e553499098747503f8d64e0912
Ignacio Rojas <igrojasro@hotmail.com>
2024-11-02 15:13:13 -0300

    Update app.py

--- app.py
+++ app.py
@@ -14,30 +14,8 @@ pelicula3 = st.text_input("Nombre película 3")
 mail = st.text_input("Ingrese correo donde recibira las recomendaciones")
 
 
-# ------------------------------------------------ #
-
-# API_KEY = "AIzaSyCLc0Gi3m6U-JgxTtu3dNgDB4N8NQ2XkPU"
-
-# genai.configure(api_key=API_KEY)
-# model = genai.GenerativeModel('gemini-1.5-flash')
-
-# def corrector(pelicula):
-#     response = model.generate_content(f"""Estrictamente, quiero que si {pelicula} esta mal escrita, solo me devuelvas el nombre de la película corregido""")
-#     # st.write(response.text)
-#     return response.text
-
-
-
 if st.button("enviar"):
     st.write("Películas Ingresadas")
-
-    # pelicula_1_corr = corrector(pelicula1)
-    # pelicula_2_corr = corrector(pelicula2)
-    # pelicula_3_corr = corrector(pelicula3)
-
-    # st.write(corrector(pelicula_1_corr))
-    # st.write(corrector(pelicula_2_corr))
-    # st.write(corrector(pelicula_3_corr))
 
     st.write(pelicula1)
     st.write(pelicula2)
