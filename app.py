ee795b5ec4f9731cc7f0c91d30eefe930a044b0f
Ignacio Rojas <igrojasro@hotmail.com>
2024-11-02 15:09:48 -0300

    Update app.py

--- app.py
+++ app.py
@@ -5,44 +5,45 @@ import google.generativeai as genai
 # un formulario, donde se ingresan las peliculas
 # Estas peliculas son guardadas en una variable
 
-peliculas_populares = [
-    "El Padrino",
-    "El Señor de los Anillos",
-    "La Lista de Schindler",
-    "El Resplandor",
-    "Pulp Fiction",
-    "La Vida es Bella",
-    "El Club de la Lucha",
-    "Forrest Gump",
-    "La Paradoja del Viajero",
-    "12 Hombres en Pugna"
-]
+st.title("Ingresar 3 películas")
 
-with st.form("formulario_peliculas"):
-    st.write("Seleccione 3 películas")
-    pelicula1 = st.selectbox("Pelicula 1", peliculas_populares)
-    pelicula2 = st.selectbox("Pelicula 2", peliculas_populares)
-    pelicula3 = st.selectbox("Pelicula 3", peliculas_populares)
-    # boton mas facil que en nuxt
-    submitted = st.form_submit_button("Enviar")
+pelicula1 = st.text_input("Nombre película 1")
+pelicula2 = st.text_input("Nombre película 2")
+pelicula3 = st.text_input("Nombre película 3")
 
-if submitted:
-    st.write(f'''
-            El usuario ingreso estas 3 peliculas \n
-            Pelicula 1: {pelicula1} \n
-            Pelicula 2: {pelicula2} \n
-            Pelicula 3: {pelicula3}        ''')
+mail = st.text_input("Ingrese correo donde recibira las recomendaciones")
 
 
-    # La parte de gimini
+# ------------------------------------------------ #
 
-    API_KEY = "AIzaSyCLc0Gi3m6U-JgxTtu3dNgDB4N8NQ2XkPU"
+# API_KEY = "AIzaSyCLc0Gi3m6U-JgxTtu3dNgDB4N8NQ2XkPU"
 
-    genai.configure(api_key=API_KEY)
+# genai.configure(api_key=API_KEY)
+# model = genai.GenerativeModel('gemini-1.5-flash')
 
-    model = genai.GenerativeModel('gemini-1.5-flash')
+# def corrector(pelicula):
+#     response = model.generate_content(f"""Estrictamente, quiero que si {pelicula} esta mal escrita, solo me devuelvas el nombre de la película corregido""")
+#     # st.write(response.text)
+#     return response.text
 
-    response = model.generate_content(f"Quiero hasta 3 recomendaciones de pelicula en base a estas 3 peliculas {pelicula1},{pelicula2},{pelicula3}")
-    st.write(response.text)
+
+
+if st.button("enviar"):
+    st.write("Películas Ingresadas")
+
+    # pelicula_1_corr = corrector(pelicula1)
+    # pelicula_2_corr = corrector(pelicula2)
+    # pelicula_3_corr = corrector(pelicula3)
+
+    # st.write(corrector(pelicula_1_corr))
+    # st.write(corrector(pelicula_2_corr))
+    # st.write(corrector(pelicula_3_corr))
+
+    st.write(pelicula1)
+    st.write(pelicula2)
+    st.write(pelicula3)
+
+
+
 
 
