efb9b6697e8ca4c55dff5d95b0f8010ed03bd82e
Ignacio Rojas <igrojasro@hotmail.com>
2024-11-03 15:08:38 -0300

    .

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import json
 # Estas peliculas son guardadas en una variable
 
 
-st.image("Imagenes/WEEKLUXE.jpeg")
+st.image("Imagenes/WEEKLUXE.jpg")
 st.title("Ingresar 3 películas")
 
 pelicula1 = st.text_input("Nombre película 1")
