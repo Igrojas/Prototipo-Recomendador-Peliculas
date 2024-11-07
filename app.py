be15142de490be1544fb589c6451f4d6ffbf21cc
Ignacio Rojas <igrojasro@hotmail.com>
2024-11-07 14:06:26 -0300

    Update app.py

--- app.py
+++ app.py
@@ -10,57 +10,34 @@ import json
 
 st.image("Imagenes/WEEKLUXE.jpg", width=300)
 
-
-# st.title("Ingresar 3 películas")
-
-# pelicula1 = st.text_input("Nombre película 1")
-# pelicula2 = st.text_input("Nombre película 2")
-# pelicula3 = st.text_input("Nombre película 3")
-
-# mail = st.text_input("Ingrese correo donde recibirá las recomendaciones")
-
-
-# if st.button("enviar"):
-#     st.write("Películas Ingresadas")
-
-#     st.write(pelicula1)
-#     st.write(pelicula2)
-#     st.write(pelicula3)
+st.write("""
+        Somos WeekLuxe, tu guía para exploradores del cine alternativo.
+         Cada semana te ofrecemos una selección curada de películas que probablemente no encontrarás fácilmente en las plataformas comunes.""")
 
 st.title("Ingresar 3 películas")
 
-# Función para restablecer los valores de los campos
-
-# Crear formulario para ingresar datos
 with st.form("formulario", clear_on_submit = True):
     pelicula1 = st.text_input("Nombre película 1", key="p1")
     pelicula2 = st.text_input("Nombre película 2", key="p2")
     pelicula3 = st.text_input("Nombre película 3", key="p3")
     mail = st.text_input("Ingrese correo", key="email")
     
-    # Botón de envío
     enviado = st.form_submit_button("Enviar")
 
-# Mostrar resultados si se hace clic en "Enviar"
 if enviado:
     st.write("Solicitud enviada: GRACIAS POR CONFIAR EN WEEKLUXE, BUEN DIA")
 
-# Obtén el diccionario de credenciales desde Streamlit secrets
 credentials_dict = st.secrets["gcp_service_account"]
 
-# Define el alcance
 scope = ["https://spreadsheets.google.com/feeds",
         "https://www.googleapis.com/auth/spreadsheets",
         "https://www.googleapis.com/auth/drive.file",
         "https://www.googleapis.com/auth/drive"]
 
-# Crea las credenciales utilizando el diccionario y el alcance
 credentials = ServiceAccountCredentials.from_json_keyfile_dict(credentials_dict, scope)
 
-# Autenticación con gspread usando las credenciales
 client = gspread.authorize(credentials)
 
-# Abrir la hoja de cálculo usando su URL
 spreadsheet_url = 'https://docs.google.com/spreadsheets/d/1T2H33PS-I0g0PhQyYblMrYcc8K3FSLbEkonV0VDYfwI/edit?usp=sharing'
 
 spreadsheet = client.open_by_url(spreadsheet_url)
@@ -72,7 +49,5 @@ pelicula_1 = pelicula1
 pelicula_2 = pelicula2
 pelicula_3 = pelicula3
 
-
-# Insertar datos en una nueva fila
 new_row = [pelicula_1, pelicula_2, pelicula_3, mail]
 worksheet_entrada.append_row(new_row)
